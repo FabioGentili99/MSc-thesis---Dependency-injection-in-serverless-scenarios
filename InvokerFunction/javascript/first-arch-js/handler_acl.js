2d3552fb1faadee0e459f9ef0e1d719648c67953
FabioGentili99 <fabiogentili20@gmail.com>
2024-12-16 20:46:19 +0100

    invokers

---
+++ InvokerFunction/javascript/first-arch-js/handler_acl.js
@@ -0,0 +1,51 @@
+const Injector = require("./Injector.js")
+const { connect } = require('nats');
+const winston = require("winston");
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({ filename: "logs.txt" }),
+  ],
+});
+
+async function hanlder(){
+
+const injector = new Injector()
+const acl_service = await injector.getServiceById("acl")
+const topic = acl_service.ServiceTopic
+const message = {user: 'Bob', permission: 'read'};
+const result = await invokeFunction(topic, message)
+
+console.log("access control result: ", result);
+process.exit(0, "access control function executed")
+}
+
+
+
+async function invokeFunction ( topic, message){
+    try {
+        // Connect to the NATS server
+        const nc = await connect({ servers: "nats://localhost:4222" }); // Replace with your NATS server address
+        
+        //console.log("Connected to NATS");
+        const start = Date.now()
+        // Publish the message
+        const response = await nc.request(topic, JSON.stringify(message));
+        const end = Date.now()
+        logger.info(`access control function executed in ${end-start} ms `)
+        // Close the connection
+        nc.drain();
+        //console.log("Connection closed");
+        return JSON.parse(response.data)
+      } catch (err) {
+        console.error("Error publishing message:", err);
+      }
+}
+
+
+hanlder();
+
---
+++ InvokerFunction/javascript/first-arch-js/handler_log.js
@@ -0,0 +1,51 @@
+const Injector = require("./Injector.js")
+const { connect } = require('nats');
+const winston = require("winston");
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json()
+    ),
+    transports: [
+      new winston.transports.File({ filename: "logs.txt" }),
+    ],
+  });
+
+async function hanlder(){
+
+const injector = new Injector()
+const log_service = await injector.getServiceById("log")
+const topic = log_service.ServiceTopic
+const message = {timestamp: "2024-11-28T16:05:34", message: "ciao", severity: "info"}
+const result = await invokeFunction(topic, message)
+
+console.log("logging result:", result)
+process.exit(0, "logging function executed")
+
+}
+
+
+
+async function invokeFunction ( topic, message){
+    try {
+        // Connect to the NATS server
+        const nc = await connect({ servers: "nats://localhost:4222" }); // Replace with your NATS server address
+        
+        const start = Date.now()
+
+        // Publish the message
+        const response = await nc.request(topic, JSON.stringify(message));
+        const end = Date.now()
+        logger.info(`logging function executed in ${end-start} ms `)
+
+        
+        // Close the connection
+        nc.drain();
+        return (response.data.toString())
+      } catch (err) {
+        console.error("Error publishing message:", err);
+      }
+}
+
+hanlder()
