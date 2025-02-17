cbd27d97185ed4fc4012d7b125e5484fb928327b
FabioGentili99 <fabiogentili20@gmail.com>
2025-02-17 20:27:45 +0100

    trigger update

--- first-architecture/Trigger/http-trigger/src/main.rs
+++ first-architecture/Trigger/http-trigger/src/main.rs
@@ -1,6 +1,7 @@
 use actix_web::{middleware, post, web::{self, Data}, App, HttpResponse, HttpServer, Responder};
 use dashmap::DashMap;
 use log::info;
+use nats::Connection;
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, env, fs::File, io::Read, time::SystemTime};
 
@@ -13,11 +14,9 @@ async fn send(
     function: String,
     map: web::Data<DashMap<String, String>>,
     req: web::Json<PublishRequest>,
+    nc: web::Data<Connection>,
 ) -> Result<(), std::io::Error> {
-    let nats_url = env::var("NATS_URL").unwrap_or("192.168.17.118:4222".to_string());
-    let nc = nats::connect(nats_url).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
     nc.publish(&map.get(&function).unwrap().to_string(), req.message.clone()).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
-    nc.close();
     Ok(())
 }
 
@@ -26,11 +25,9 @@ async fn query(
     function: String,
     map: web::Data<DashMap<String, String>>,
     req: web::Json<PublishRequest>,
+    nc: web::Data<Connection>
 ) -> Result<nats::Message, std::io::Error> {
-    let nats_url = env::var("NATS_URL").unwrap_or("192.168.17.118:4222".to_string());
-    let nc = nats::connect(nats_url).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
     let response = nc.request(&map.get(&function).unwrap().to_string(), req.message.clone()).unwrap();
-    nc.close();
     Ok(response)
 }
 
@@ -40,9 +37,10 @@ async fn async_invoke(
     routes: web::Data<DashMap<String, String>>,
     req: web::Json<PublishRequest>,
     fun: web::Path<String>,
+    nc: web::Data<Connection>,
 ) -> impl Responder {
     let function_name = fun.into_inner();
-    let _ = send(function_name.clone(), routes, req).await;
+    let _ = send(function_name.clone(), routes, req, nc).await;
     info!("handling request to async function: {}", function_name);
     HttpResponse::Ok().body("ok")
 }
@@ -52,10 +50,11 @@ async fn sync_invoke(
     routes: web::Data<DashMap<String, String>>,
     req: web::Json<PublishRequest>,
     fun: web::Path<String>,
+    nc: web::Data<Connection>,
 ) -> impl Responder {
     let sys_time = SystemTime::now();
     let function_name = fun.into_inner();
-    let message = query(function_name.clone(), routes, req).await.unwrap();
+    let message = query(function_name.clone(), routes, req, nc).await.unwrap();
     let new_sys_time = SystemTime::now();
     let difference = new_sys_time
                     .duration_since(sys_time)
@@ -66,6 +65,7 @@ async fn sync_invoke(
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let nats_url: String = env::var("NATS_URL").unwrap_or("192.168.17.118:4222".to_string());
     log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
     let routes: Data<DashMap<String, String>> = Data::new(DashMap::new());
     let mut file = File::open("config.json")?; // Open the file
@@ -78,13 +78,17 @@ async fn main() -> std::io::Result<()> {
         routes.insert(key, value);
     };
 
+    let nc = nats::connect(nats_url).unwrap();
     HttpServer::new(move || {
         App::new()
             .app_data(routes.clone())
+            .data(nc.clone())
             .service(sync_invoke)
             .service(async_invoke)
             .wrap(middleware::Logger::default())
+            
     })
+    .workers(20)
     .bind(("0.0.0.0", 8081))?
     .run()
     .await
